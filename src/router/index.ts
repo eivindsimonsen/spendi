e44ed0a1137be0836c8dd59db92643c525221569
Eivind Simonsen <74544541+eivindsimonsen@users.noreply.github.com>
2026-01-04 15:36:05 +0100

    Router set up

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,48 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [
-  {
-    path: '/',
-    name: 'Oversikt',
-    component: Overview,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/household',
-    name: 'Husholdning',
-    component: Household,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/user',
-    name: 'Bruker',
-    component: User,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
-];
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'overview',
+      component: () => import('../views/OverviewView.vue'),
+    },
+    {
+      path: '/budgets',
+      name: 'budgets',
+      component: () => import('../views/BudgetsView.vue'),
+    },
+    {
+      path: '/household',
+      name: 'household',
+      component: () => import('../views/HouseholdView.vue'),
+    },
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('../views/AccountView.vue'),
+    },
+  ],
+})
 
-// Navigation guard
-router.beforeEach((to, from, next) => {
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
-  
-  if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/login');
-  } else if (to.path === '/login' && isAuthenticated) {
-    next('/');
-  } else {
-    next();
-  }
-});
-
-export default router;
-
+export default router
